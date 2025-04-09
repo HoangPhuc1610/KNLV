a30af20325ea4ba1bd52e2bef59b7bcba1d632f1
HoangPhuc1610 <phucvh250@gmail.com>
2025-04-09 23:23:34 +0700

    sp yêu thích

--- ASM2_KNLV/backend/routes/users.js
+++ ASM2_KNLV/backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 
-const{register,login,getUser,verifyToken,verifyAdmin,addFavorite, checkFavorite, getAllUsers}=require('../controllers/userController');
+const{register,login,getUser,verifyToken,verifyAdmin,addFavorite, getFavorites,checkFavorite, getAllUsers, removeFavorite}=require('../controllers/userController');
 
 
 //dăng kí người dùng
@@ -12,7 +12,12 @@ router.post('/register',register);
 router.post('/login',login);
 router.get('/getuser',verifyToken, verifyAdmin, getUser);
 router.post('/:email/favorite/:productId', addFavorite);
-router.get('/users/:email/favorite/:productId', checkFavorite);
+router.get('/:email/favorites', getFavorites);
+router.get('/:email/favorite/:productId', checkFavorite);
+router.delete('/:email/favorite/:productId', removeFavorite);
+
+
+
 
 
 
