f53d8e4d2d6b252e7eb9e03a84029a0b33691ba7
huynhvu1304 <hvu1436@gmail.com>
2025-04-10 21:05:25 +0700

    gioithieu

--- ASM2_KNLV/site/src/app/timkiem/timkiem.component.ts
+++ ASM2_KNLV/site/src/app/timkiem/timkiem.component.ts
@@ -3,23 +3,29 @@ import { ListcardComponent } from '../listcard/listcard.component';
 import { ProductInterface } from '../product-interface';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-timkiem',
-  imports: [ListcardComponent],
+  imports: [ListcardComponent,CommonModule],
   templateUrl: './timkiem.component.html',
   styleUrl: './timkiem.component.css'
 })
 export class TimkiemComponent {
   searchPro: ProductInterface[] = [];
+  noResults: boolean = false; // Thêm biến để kiểm tra không có kết quả
+
   constructor(
     private productService: ProductService,
-    private route:ActivatedRoute
+    private route: ActivatedRoute
   ) {}  
-    ngOnInit() {
-      this.route.queryParams.subscribe((params) => {
-        this.productService.searchProducts(params['ten_mon']).subscribe((data) => {
-          this.searchPro = data;
-        });
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.productService.searchProducts(params['ten_mon']).subscribe((data) => {
+        this.searchPro = data;
+        this.noResults = this.searchPro.length === 0; // Kiểm tra nếu mảng rỗng
       });
-    }
+    });
+  }
 }
