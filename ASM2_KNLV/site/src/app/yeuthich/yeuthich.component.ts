f0a3ba09bc8301ae128ee7b3d040a7af2262e740
Trieu11092005 <trieuhqps39434@gmail.com>
2025-04-19 21:55:32 +0700

    reponsive

--- ASM2_KNLV/site/src/app/yeuthich/yeuthich.component.ts
+++ ASM2_KNLV/site/src/app/yeuthich/yeuthich.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { environment } from '../environments/environment'; // Import environment
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-yeuthich',
@@ -12,30 +12,30 @@ import { environment } from '../environments/environment'; // Import environment
 })
 export class YeuthichComponent implements OnInit {
   favoriteProducts: any[] = [];
-  email = localStorage.getItem('email'); // email user đăng nhập
-  environment = environment; 
+  email = localStorage.getItem('email');
+  environment = environment;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     const email = localStorage.getItem('email');
-    this.http.get(`${environment.apiUrl}/users/${email}/favorites`) // Sử dụng apiUrl từ environment
+    this.http.get(`${environment.apiUrl}/users/${email}/favorites`)
       .subscribe((res: any) => {
-        this.favoriteProducts = res.favoriteProducts;
+        this.favoriteProducts = res.favoriteProducts || [];
       });
   }
 
   removeFromFavorite(productId: string) {
-    const email = localStorage.getItem('email'); // hoặc từ service nếu bạn lưu email ở đó
-  
+    const email = localStorage.getItem('email');
+
     if (!email) {
       alert('Không tìm thấy email người dùng!');
       return;
     }
-  
-    this.http.delete(`${environment.apiUrl}/users/${email}/favorite/${productId}`) // Sử dụng apiUrl từ environment
+
+    this.http.delete(`${environment.apiUrl}/users/${email}/favorite/${productId}`)
       .subscribe({
         next: () => {
-          // Sau khi xóa, cập nhật lại danh sách yêu thích
           this.favoriteProducts = this.favoriteProducts.filter(item => item._id !== productId);
         },
         error: (err) => {
